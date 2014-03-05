09e6339f1fd36755204dd15d4e6b7374bd176ebe
Silumesii Maboshe <silumesii@pencilcasestudios.com>
2014-03-05 11:03:25 +0200

    Setting up meta tags and i18n to do titles, open graph and meta tags.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,24 @@
 module ApplicationHelper
+  def body_id
+    controller.controller_name
+  end
+
+  # Ref: https://github.com/kpumuk/meta-tags
+  def default_meta_tags
+    {
+      description: t("application.meta_tags.description"),
+      keywords: t("application.meta_tags.keywords"),
+      prefix: " ",
+      reverse: true,
+      site: t("application.name"),
+      title: t("application.tagline_html"),
+      open_graph: {
+        description: t("application.meta_tags.description"),
+        image: root_url + asset_path("logos/galaunia_farms_logo-400x400.png"),
+        title: t("application.tagline_html"),
+        type: :website,
+        url: root_url,
+      }
+    }
+  end
 end
