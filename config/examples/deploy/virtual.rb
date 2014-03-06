cec46f3fe3f9e77a7e2355c194d6f7412cd412f1
Silumesii Maboshe <silumesii@pencilcasestudios.com>
2014-03-06 10:28:07 +0200

    Using the Capistrano 3 fetch syntax for the example stage deployment configurations.

--- config/examples/deploy/virtual.rb
+++ config/examples/deploy/virtual.rb
@@ -1,11 +1,11 @@
 # Ref: http://gist.github.com/293302
-server DEPLOYMENT_CONFIG["virtual_server_name"], :web, :app, :db, primary: true
+server DEPLOYMENT_CONFIG["virtual_server_name"], roles: [:web, :app, :db]
 set :deployment_path, DEPLOYMENT_CONFIG["virtual_deployment_path"]
-set :deploy_to, "#{applications_folder}/#{application}/#{deployment_path}"
+set :deploy_to, "#{fetch(:applications_folder)}/#{fetch(:application)}/#{fetch(:deployment_path)}"
 
 # Don't forget to make this repo on the server
 set :repository_server_name, DEPLOYMENT_CONFIG["virtual_server_name"]
-set :repo_url, "#{user}@#{repository_server_name}:/var/Repositories/Git/#{application}.git"
+set :repo_url, "#{fetch(:user)}@#{repository_server_name}:/var/Repositories/Git/#{fetch(:application)}.git"
 # Don't forget to make this branch in the repository
 set :branch, DEPLOYMENT_CONFIG["virtual_repository_deployment_branch"]
 
@@ -14,8 +14,8 @@ set :branch, DEPLOYMENT_CONFIG["virtual_repository_deployment_branch"]
 
 # Deployment dependencies
 # Ref: https://github.com/capistrano/capistrano/wiki/Capistrano-Tasks#deploycheck
-depend :remote, :writable, "#{asset_vault_path}/#{application}/Images/"
-depend :remote, :writable, "#{asset_vault_path}/#{application}/PDFs/"
+depend :remote, :writable, "#{fetch(:asset_vault_path)}/#{fetch(:application)}/Images/"
+depend :remote, :writable, "#{fetch(:asset_vault_path)}/#{fetch(:application)}/PDFs/"
 
 
 
@@ -24,11 +24,11 @@ namespace :deploy do
   desc "Synchronise assets"
   task :sync_assets do
     # Upload assets to the AssetsVault
-    run_locally "rsync --verbose --stats --progress --compress --archive --partial --recursive --times --perms --links --delete --exclude '.*' --delete-excluded ~/Projects/Rails/Galaunia/app/assets/images/ #{user}@#{DEPLOYMENT_CONFIG["virtual_server_name"]}:#{asset_vault_path}/#{application}/Images/"
-    run_locally "rsync --verbose --stats --progress --compress --archive --partial --recursive --times --perms --links --delete --exclude '.*' --delete-excluded ~/Projects/Rails/Galaunia/app/assets/pdfs/ #{user}@#{DEPLOYMENT_CONFIG["virtual_server_name"]}:#{asset_vault_path}/#{application}/PDFs/"
+    run_locally "rsync --verbose --stats --progress --compress --archive --partial --recursive --times --perms --links --delete --exclude '.*' --delete-excluded ~/Projects/Rails/Galaunia/app/assets/images/ #{fetch(:user)}@#{DEPLOYMENT_CONFIG["virtual_server_name"]}:#{fetch(:asset_vault_path)}/#{fetch(:application)}/Images/"
+    run_locally "rsync --verbose --stats --progress --compress --archive --partial --recursive --times --perms --links --delete --exclude '.*' --delete-excluded ~/Projects/Rails/Galaunia/app/assets/pdfs/ #{fetch(:user)}@#{DEPLOYMENT_CONFIG["virtual_server_name"]}:#{fetch(:asset_vault_path)}/#{fetch(:application)}/PDFs/"
 
     # Copy assets from the AssetsVault to the shared assets path for the deployed application
-    run "rsync --verbose --stats --progress --compress --archive --partial --recursive --times --perms --links --delete --exclude '.*' --delete-excluded #{asset_vault_path}/#{application}/Images/ #{shared_path}/images/"
-    run "rsync --verbose --stats --progress --compress --archive --partial --recursive --times --perms --links --delete --exclude '.*' --delete-excluded #{asset_vault_path}/#{application}/PDFs/ #{shared_path}/pdfs/"
+    run "rsync --verbose --stats --progress --compress --archive --partial --recursive --times --perms --links --delete --exclude '.*' --delete-excluded #{fetch(:asset_vault_path)}/#{fetch(:application)}/Images/ #{fetch(:shared_path)}/images/"
+    run "rsync --verbose --stats --progress --compress --archive --partial --recursive --times --perms --links --delete --exclude '.*' --delete-excluded #{fetch(:asset_vault_path)}/#{fetch(:application)}/PDFs/ #{fetch(:shared_path)}/pdfs/"
   end
 end
