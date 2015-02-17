70dfd6b248bf823db0217b0c49c197b4daa45fdd
Silumesii Maboshe <silumesii@pencilcasestudios.com>
2015-02-17 15:00:50 +0200

    Updating to Rails 4.2 configurations.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-IEmerge::Application.config.session_store :cookie_store, key: '_iEmerge_session'
+Rails.application.config.session_store :cookie_store, key: '_i_emerge_session'
